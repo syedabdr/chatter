0e038d252626d80678d2aba62b356239ca9239fb
MuhammadAbdullahButt <abdullahkabirbutt2002@gmail.com>
2023-10-13 20:14:13 +0500

    Group Make

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -14,7 +14,7 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
     return (
         <div className="channel-name-input__wrapper">
             <p>Name</p>
-            <input value={channelName} onChange={handleChange} placeholder="channel-name" />
+            <input value={channelName} onChange={handleChange} placeholder="Group Name" />
             <p>Add Members</p>
         </div>
     )
@@ -47,13 +47,13 @@ const CreateChannel = ({ createType, setIsCreating }) => {
     return (
         <div className="create-channel__container">
             <div className="create-channel__header">
-                <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
+                <p>{createType === 'team' ? 'Create a New Group' : 'Send a Direct Message'}</p>
                 <CloseCreateChannel setIsCreating={setIsCreating} />
             </div>
             {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
             <UserList setSelectedUsers={setSelectedUsers} />
             <div className="create-channel__button-wrapper" onClick={createChannel}>
-                <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
+                <p>{createType === 'team' ? 'Create Group' : 'Create Message Group'}</p>
             </div>
         </div>
     )
