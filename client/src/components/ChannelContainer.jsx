552677d8b27c139b8e0e6907a859b1d40bed220e
MuhammadAbdullahButt <abdullahkabirbutt2002@gmail.com>
2023-11-22 12:47:57 +0500

    ChannelContainer

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -4,6 +4,7 @@ import { Channel, MessageSimple } from 'stream-chat-react';
 import { ChannelInner, CreateChannel, EditChannel } from './';
 
 const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    // Render the component for creating a new channel
     if(isCreating) {
         return (
             <div className="channel__container">
@@ -11,7 +12,7 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
             </div>
         )
     }
-
+ // Render the component for editing an existing channel
     if(isEditing) {
         return (
             <div className="channel__container">
@@ -19,14 +20,14 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
             </div> 
         )
     }
-
+ // EmptyState component to be displayed when there are no messages in the channel
     const EmptyState = () => (
         <div className="channel-empty__container">
             <p className="channel-empty__first">This is the beginning of your chat history.</p>
             <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
         </div>
     )
-
+    // Render the main channel component with Stream Chat components
     return (
         <div className=" channel__container">
             <Channel
